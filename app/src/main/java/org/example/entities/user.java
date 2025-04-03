fec5e8e07e55941a2f001a64cfbf33823591563d
AtharvAwle <atharvawle0156@gmail.com>
2025-04-03 16:48:35 +0530

    Backend entities initilised

---
+++ app/src/main/java/org/example/entities/user.java
@@ -0,0 +1,9 @@
+package org.example.entities;
+import java.util.*;
+public class user {
+    private String name;
+    private String password;
+    private String hashPassword;
+    private String usedId;
+    private List<Ticket> ticketBooked;
+}
